55348c15335fcb3366f53b9eaa2f0d39220fed67
tomassanfelipe <tommisanfe@gmail.com>
2025-05-05 16:14:34 -0300

    agregue validaciones

--- main/proceso_numeros.py
+++ main/proceso_numeros.py
@@ -4,6 +4,9 @@ def procesar_uniforme(n_string, a_string, b_string):
     n = int(n_string)
     a = int(a_string)
     b = int(b_string)
+    
+    if n <= 0 or n > 1000000:
+        raise ValueError("La cantidad de muestras debe ser mayor que cero y hasta 1000000.")
 
     if a >= b:
         raise ValueError("El valor A debe ser menor que el valor B.")
@@ -13,6 +16,9 @@ def procesar_normal(n_string, media_string, desviacion_string):
     n = int(n_string)
     media = int(media_string)
     desviacion = int(desviacion_string)
+    
+    if n <= 0 or n > 1000000:
+        raise ValueError("La cantidad de muestras debe ser mayor que cero y hasta 1000000.")
 
     if desviacion <= 0:
         raise ValueError("La desviación estándar debe ser mayor que cero.")    
@@ -21,6 +27,9 @@ def procesar_normal(n_string, media_string, desviacion_string):
 def procesar_exponencial(n_string, media_string):
     n = int(n_string)
     media = int(media_string)
+    
+    if n <= 0 or n > 1000000:
+        raise ValueError("La cantidad de muestras debe ser mayor que cero y hasta 1000000.")
 
     if media <= 0:
         raise ValueError("La media debe ser mayor que cero.")
