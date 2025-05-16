508a346751667593379125dd9436e2c970f3eca3
tomassanfelipe <tommisanfe@gmail.com>
2025-05-15 21:42:16 -0300

    agrego redondeo 4 decimales

--- main/generar_numeros.py
+++ main/generar_numeros.py
@@ -5,20 +5,21 @@ from exponencial import exponencial
 def generar_numeros(distribucion, n, parametros):
     if distribucion == 'Uniforme':
         a, b = parametros
-        numeros = [uniforme(a, b) for i in range(n)]
+        numeros = [round(uniforme(a, b), 4) for i in range(n)]
 
     elif distribucion == 'Exponencial':
         lambd = parametros[0]
-        numeros = [exponencial(lambd) for i in range(n)]
+        numeros = [round(exponencial(lambd), 4) for i in range(n)]
 
     elif distribucion == 'Normal':
         media, desviacion = parametros
         numeros = []
         for i in range(n // 2): # con n//2 aseguro que se generen dos numeros por iteracion
             n1, n2 = normal(media, desviacion)
-            numeros.extend([n1, n2])
+            numeros.extend([round(n1, 4), round(n2, 4)])
         if n % 2 != 0:  # aca verifica que si no son pares
             n1, i = normal(media, desviacion) # genera un numero adicional
-            numeros.append(n1) # solo agrega un valor de los dos que se generan 
+            numeros.append(round(n1, 4)) # solo agrega un valor de los dos que se generan 
+            
 
     return numeros
